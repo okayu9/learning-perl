cffdf0593bfa09d54b61b33a2f80ccb162938bdb
Yumeto Inaoka <in@okayu.jp>
2017-09-15 01:52:05 +0900

    lesson10を学習
    条件分岐について

---
+++ l10-if.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+# eq ne gt lt ge le
+# 0-9 < A-Z < a-z
+# && || !
+
+my $name = "sakura";
+my $gender = "female";
+if(($name eq "sakura") && ($gender eq "female")){
+  print("Mai Waifu");
+}
+
+
