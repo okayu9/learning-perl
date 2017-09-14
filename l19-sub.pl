e72edb898fbbbde4aa91dbf1532c51133e085c1a
Yumeto Inaoka <in@okayu.jp>
2017-09-15 04:02:09 +0900

    lesson19を学習
    サブルーチンについて

---
+++ l19-sub.pl
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+
+sub booby{
+  my $min = shift(@_);
+  my $min_second = shift(@_);
+  if ($min_second < $min){
+    my $tmp = $min;
+    $min = $min_second;
+    $min_second = $tmp;
+  }
+  for (@_){
+    if ($_ < $min){
+      $min_second = $min;
+      $min = $_;
+    } elsif ($_ < $min_second){
+      $min_second = $_;
+    }
+  }
+  $min_second;
+}
+
+print booby(12, 3, 45, 6, 78, 9)
+
