61dd3c94ac54917fe14f249770a45c36dbaae80a
Yumeto Inaoka <in@okayu.jp>
2017-09-15 01:58:14 +0900

    lesson11を学習
    条件分岐について

---
+++ l11-if.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+my $name = "honoka";
+my $gender = "female";
+
+if(($name eq "sakura") && ($gender eq "female")){
+  print("Mai Waifu");
+} elsif ($name eq "honoka"){
+  print("Idol");
+} else{
+  print("dunno");
+}
+
