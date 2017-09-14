16e85443bd68539f4f36aba38599cbf3913e1b93
Yumeto Inaoka <in@okayu.jp>
2017-09-14 23:54:36 +0900

    lesson4を学習
    数値の扱いについて

---
+++ l04-numeric_value.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+my $year = 1984;
+my $height = 129.3;
+my $population = 126_754_740;
+
+my $x = 10 * 10; # 100
+my $y = 10 % 3; # 1
+my $z = 2 ** 3; # 8
+
+$x++;
+$y--;
+
+$x += 5; # $x = $x + 5
+
