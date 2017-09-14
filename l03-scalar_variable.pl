09498d8be1ba7694c5e225da5ee46f8b1d81c515
Yumeto Inaoka <in@okayu.jp>
2017-09-14 23:40:26 +0900

    lesson3を学習
    スカラー変数について

---
+++ l03-scalar_variable.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+# スカラー変数には数値や文字列が入る
+# スカラー変数の先頭は$
+# 変数を宣言するときはmyを付ける
+
+my $greet;
+$greet = "Hello, Perl!!\n";
+print $greet;
+
+my $msg = "Perl is a piece of cake.\n";
+print $msg;
